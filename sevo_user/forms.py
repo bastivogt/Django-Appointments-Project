fed0a5c2bd527b6b8f1db6a6a82eea607fdffd5f
Sevo <sevodo.dessau@gmail.com>
2025-04-05 02:23:13 +0200

    changes at sevo_auth

--- sevo_user/forms.py
+++ sevo_user/forms.py
@@ -6,7 +6,9 @@ User = get_user_model()
 
 
 class SignUpForm(UserCreationForm):
-    
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     class Meta:
         model = User
         fields = [
